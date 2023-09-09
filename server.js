fd396d1735da087a8ffdfc2662a2122e6a8a406a
yusuf abdulsamad <yusufabdulsamad93@gmail.com>
2023-09-09 20:25:37 +0100

    te

--- server.js
+++ server.js
@@ -1,9 +1,66 @@
+// const express = require('express');
+// const app = express();
+
+// app.get('/api', (req, res) => {
+
+//     const currentDate = now.toISOString().slice(0, 19) + 'Z';
+//     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//     const currentDay = daysOfWeek[new Date().getDay()];
+
+//     // Get the current UTC time
+//     const now = new Date();
+//     now.setMinutes(now.getMinutes() - 2); // Adjust for +/-2 minutes
+//     const utcTime = now.toISOString();
+
+
+//     //git file
+//     const githubFileUrl = 'https://github.com/samad13/HNG/blob/master/server.js';
+
+//     //git repo
+//     const githubRepoUrl = 'https://github.com/samad13/HNG';
+
+//     const response = {
+//         slack_name,
+//         current_day: currentDay,
+//         utc_time: currentDate,
+//         track,
+//         github_file_url: githubFileUrl,
+//         github_repo_url: githubRepoUrl,
+//         status_code: 200
+//     }
+//     res.json(response)
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const port = process.env.PORT || 3000
+
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
+
+
+
+
 const express = require('express');
 const app = express();
 
+// Define a route that handles GET requests
 app.get('/api', (req, res) => {
+    // Parse query parameters
+    const { slack_name, track } = req.query;
 
-    const currentDate = now.toISOString().slice(0, 19) + 'Z';
+    // Get the current day of the week
     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const currentDay = daysOfWeek[new Date().getDay()];
 
@@ -12,39 +69,23 @@ app.get('/api', (req, res) => {
     now.setMinutes(now.getMinutes() - 2); // Adjust for +/-2 minutes
     const utcTime = now.toISOString();
 
-
-    //git file
-    const githubFileUrl = 'https://github.com/samad13/HNG/blob/master/server.js';
-
-    //git repo
-    const githubRepoUrl = 'https://github.com/samad13/HNG';
-
+    // Construct the JSON response
     const response = {
         slack_name,
         current_day: currentDay,
-        utc_time: currentDate,
+        utc_time: utcTime,
         track,
-        github_file_url: githubFileUrl,
-        github_repo_url: githubRepoUrl,
-        status_code: 200
-    }
-    res.json(response)
-})
+        github_file_url: ''https://github.com/samad13/HNG/blob/master/server.js',
+            github_repo_url: 'https://github.com/samad13/HNG',
+        status_code: 200,
+    };
 
+    // Send the JSON response
+    res.json(response);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-const port = process.env.PORT || 3000
-
+// Start the server
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
