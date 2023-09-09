04b3575aaafe62c0249b28fa07921ad47a2f9f44
S13 <61961655+samad13@users.noreply.github.com>
2023-09-09 19:34:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.get('/api', (req, res) => {
-    const { slack_name, track } = req.query;
-    const daysOfTheWeek = ['Sunday', 'Monday', "Tuesday", "Wednesday", "Thursday", "FRiday", "Saturday"];
-
-    const currentDay = new Date().toLocaleString('en-US', { weekday: 'long' });
-    const now = new Date();
-
+app.get('/api', (req, res) => {  
 
     const currentDate = now.toISOString().slice(0, 19) + 'Z';
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const currentDay = daysOfWeek[new Date().getDay()];
+
+  // Get the current UTC time
+  const now = new Date();
+  now.setMinutes(now.getMinutes() - 2); // Adjust for +/-2 minutes
+  const utcTime = now.toISOString();
 
     //git file
     const githubFileUrl = 'https://github.com/samad13/HNG/blob/master/server.js';
@@ -18,10 +19,10 @@ app.get('/api', (req, res) => {
     const githubRepoUrl = 'https://github.com/samad13/HNG';
 
     const response = {
-        slack_name,
-        currentDay: currentDay,
-        utc_time: currentDate,
-        track,
+        slack_name:req.query,
+        current_Day: currentDay,
+        utc_time: utcTime,
+        track:req.query,
         github_file_url: githubFileUrl,
         github_repo_url: githubRepoUrl,
         status_code: 200
