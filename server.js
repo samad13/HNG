18263e5b90fecab81ba5a1980eac128f4d493b65
S13 <61961655+samad13@users.noreply.github.com>
2023-09-10 02:39:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 const moment = require('moment')
+const headSetter = (req, res, next) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.setHeader('Access-Control-Allow-Origin', '*')
 
+    next()
+}
+
+app.use(headSetter)
 
 app.get('/api', (req, res) => {
     const { slack_name, track } = req.query
@@ -13,10 +20,10 @@ app.get('/api', (req, res) => {
     const github_file_url = 'https://github.com/samad13/HNG/blob/master/server.js'
     const github_repo_url = 'https://github.com/samad13/HNG'
     const responseObject = {
-        slack_name,
+        slack_name:`${slack_name}`,
         current_day: weekDays[current_day],
         utc_time,
-        track,
+        track:`${track}`,
         github_file_url,
         github_repo_url,
         status_code: 200
